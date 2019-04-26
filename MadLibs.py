5d15c416b9b333fa6243c65b4176486c50c0de48
Gobbledy <49748273+Gobbledy@users.noreply.github.com>
2019-04-26 15:58:48 +0200

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,5 @@
+def mad_lib():
+    name = input('Enter a name: ')  
+    noun = input('Enter an object: ')
+    print(name + ' went for a walk in the park and found a ' + noun + '. ' + name + ' decided to take it home.')
+mad_lib()
